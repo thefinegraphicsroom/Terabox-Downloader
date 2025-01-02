6b90704c42172ccb504145f7d7a15370d64c8e25
HmmSmokieOfficial <Hmmsmokieofficial@gmail.com>
2025-01-02 14:15:49 +0530

    Update Terabox.py

--- Terabox.py
+++ Terabox.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 # Bot Configuration
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "7964395750:AAHtEjjIL-7i-iX7rTmj2lR_AVmL46J8L7s"
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 ADMIN_IDS = [1949883614]  # Directly define the list of admin IDs
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
